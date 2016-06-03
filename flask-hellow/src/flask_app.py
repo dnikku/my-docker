371f8808055e59f2b67a9d82ddcfc2f10e9a8129
Ubuntu <nicu.dascalu@gmail.com>
2016-06-03 11:55:10 +0100

    add supervisor for gunicorn flask hellow

--- flask-hellow/src/flask_app.py
+++ flask-hellow/src/flask_app.py
@@ -1,18 +1,25 @@
 import sys
 import logging
+import pprint
+from datetime import datetime
+
 logging.basicConfig(stream=sys.stdout)
 
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World (root)!"
 
-@app.route("/route2/")
+@app.route("/hh/")
 def hello2():
-    return "Hello World (/route2/)!"
+    out = pprint.pformat(request.__dict__)
+    out = ("Date: %r\n\n" % datetime.utcnow()) + out
+    out = out.replace("\n", "<br>\n")
+    return "<h1>Hello World (/hh/)!</h1>" + out
 
 
 logging.info("Flask hellow created")
--- flask-hellow/src/start.sh
+++ flask-hellow/src/start.sh
@@ -7,5 +7,4 @@ cp ./nginx.conf /etc/nginx/nginx.conf
 nginx -c /etc/nginx/nginx.conf
 
 #start supervisor
-#supervisord -c /home/oa2b/supervisord.conf
-gunicorn -w 1 -b 127.0.0.1:8001 flask_app:app
+supervisord --nodaemon -c ./supervisord.ini
