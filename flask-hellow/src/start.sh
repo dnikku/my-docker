ac27141b7945b065ec24fc7d0daa60b546c2ff6e
Ubuntu <nicu.dascalu@gmail.com>
2016-06-03 14:35:46 +0100

    forward nginx logs to docker log collector

--- flask-hellow/src/start.sh
+++ flask-hellow/src/start.sh
@@ -2,6 +2,12 @@
 
 cd /home/hellow
 
+# forward request and error logs to docker log collector
+mkdir -p /var/log/nginx
+rm /var/log/ngingx/*.log
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
+
 #start nginx as daemon
 cp ./nginx.conf /etc/nginx/nginx.conf
 nginx -c /etc/nginx/nginx.conf
